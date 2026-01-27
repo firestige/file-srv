e91c365e6ceb68089387abf722e86e85e55e6f54
firestige <zzt.nbl@hotmail.com>
2026-01-27 19:43:38 +0800

    feat: 完善辅助模块 - plugins、autoconfiguration、test
    plugins 模块:
    - RenamePlugin: 示例插件实现 SharedPlugin 接口
    - RenameAutoconfiguration: 插件自动配置
    
    autoconfiguration 模块:
    - FileServiceAutoConfiguration: 服务自动配置入口
    
    test 模块:
    - Bootstrap: Spring Boot 启动类

--- file-srv-plugins/file-srv-plugin-example/src/main/java/tech/icc/filesrv/plugin/example/RenamePlugin.java
+++ file-srv-plugins/file-srv-plugin-example/src/main/java/tech/icc/filesrv/plugin/example/RenamePlugin.java
@@ -1,4 +1,21 @@
 package tech.icc.filesrv.plugin.example;
 
-public class RenamePlugin {
+import tech.icc.filesrv.common.context.TaskContext;
+import tech.icc.filesrv.core.infra.plugin.SharedPlugin;
+
+public class RenamePlugin implements SharedPlugin {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void apply(TaskContext ctx) {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
 }
--- file-srv-test/src/main/java/tech/icc/filesrv/Bootstrap.java
+++ file-srv-test/src/main/java/tech/icc/filesrv/Bootstrap.java
@@ -1,4 +1,11 @@
 package tech.icc.filesrv;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Bootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
 }
